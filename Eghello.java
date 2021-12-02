e62cd16e7fc77d8f1758554c99bbf7ded360350a
Chodisettyanupama <91118800+Chodisettyanupama@users.noreply.github.com>
2021-12-02 10:45:34 +0530

    Add files via upload

---
+++ Eghello.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PEC-IT
+ */
+public class Eghello 
+{
+    public static void main(String args[])
+    { 
+        int a=10,b=20,c=0;
+        c=a+b;
+        System.out.println("addition is "+c);
+    }
+    
+}
